04fb8f5427182f92f93a3d78ad0d86a45f2a3b36
ShouTzu, Han <debra4117@gmail.com>
2022-01-13 15:35:39 +0800

    update

--- src/linkedlistpractice3/LinkedListIterator.java
+++ src/linkedlistpractice3/LinkedListIterator.java
@@ -168,6 +168,51 @@ package linkedlistpractice3;
  *
  */
 
-public class LinkedListIterator {
+// LinkedListIterator class; maintains "current position"
+//
+// Construction: Package visible only, with a ListNode
+// 
+// Public operations:
+// void advance()     -----> Advance
+// boolean isValid()  -----> True if at valid position in list
+// AnyType retrieve() -----> Return item in current position
+
+
+public class LinkedListIterator<AnyType> {
+	
+	ListNode<AnyType> current; // Current position
+	
+	/**
+	 * Construct the list iterator
+	 * @param theNode any node in the linked list.
+	 */
+	LinkedListIterator(ListNode<AnyType> theNode){
+		current = theNode;
+	}
+	
+	/**
+	 * Test if the current position is a valid position in the list.
+	 * @return true if the current position is valid.
+	 */
+	public boolean isValid() {
+		return current != null;
+	}
+	
+	/**
+	 * Return the item stored in the current position.
+	 * @return the stored item or null if the current position is not in the list.
+	 */
+	public AnyType retrieve() {
+		return isValid() ? current.element : null; 
+	}
+	
+	/**
+	 * Advance the current position to the next node in the list.
+	 * If the current position is null, then do nothing.
+	 */
+	public void advance() {
+		if(isValid())
+			current = current.next;
+	}
 
 }
